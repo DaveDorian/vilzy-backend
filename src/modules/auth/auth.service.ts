b723ffc69b1ed90b242de8ad0444f42c9a128274
davemark2 <davedorianm@gmail.com>
2026-02-13 00:46:17 -0400

    feat: Se agrego el recurso auth

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(data: any) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        surname: data.surname,
+        email: data.email,
+        ci: data.ci,
+        password: hashedPassword,
+        role: data.role,
+      },
+    });
+
+    return this.generateToken(user);
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) throw new UnauthorizedException('Credenciales Invalidas');
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      throw new UnauthorizedException('Contraseña Incorrecta');
+
+    return this.generateToken(user);
+  }
+
+  private generateToken(user: any) {
+    return {
+      access_token: this.jwtService.sign({
+        sub: user.id,
+        role: user.role,
+      }),
+    };
+  }
+}
