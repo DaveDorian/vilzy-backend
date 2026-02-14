963b8f30cafb98abe5def3682ef0cd54f0b5d36f
davemark2 <davedorianm@gmail.com>
2026-02-14 14:30:43 -0400

    feat: Se agrego autenticacion

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'supersecret',
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
