bcfb7f81a6d30fbc2b4f8e09b68b3da82089c047
davemark2 <davedorianm@gmail.com>
2026-02-17 22:53:48 -0400

    feat: Se agrego policies dinamicas (ABAC)

---
+++ src/common/policies/user.policy.ts
@@ -0,0 +1,14 @@
+import { Role } from 'generated/prisma/enums';
+
+export class UserPolicy {
+  static canUpdate(currentUser: any, targetUserId: number): boolean {
+    if (currentUser.role === Role.SUPER_ADMIN) return true;
+
+    if (currentUser.role === Role.ADMIN) return true;
+
+    if (currentUser.role === Role.CLIENT)
+      return currentUser.sub === targetUserId;
+
+    return false;
+  }
+}
